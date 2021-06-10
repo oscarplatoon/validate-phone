798dcd239390fbb5511215c6780be7f190e33a6f
RenoAverill <reno_averill@yahoo.com>
2021-06-10 07:25:42 -0600

    Reno's validate_phone.py

--- validate_phone.py
+++ validate_phone.py
@@ -1,20 +1,31 @@
+import re
 # Does a string contain a phone number?
 def has_phone_number(input_string):
-    pass 
-
+    pattern = "\d{3}-\d{3}-\d{4}"
+    number = re.search(pattern,input_string)
+    if number == None:
+       return False
+    return True
 # Get a phone number back from a string
 def get_phone_number(input_string):
-    pass
-
+    pattern = "\d{3}-\d{3}-\d{4}"
+    number = re.search(pattern, input_string)
+    if number == None:
+        return None
+    return number.group()
+    
 
 # Gets and returns all phone numbers from an inputed string
 def get_all_phone_numbers(input_string):
-    pass
+    pattern = "\d{3}-\d{3}-\d{4}"
+    phone_numbers = re.findall(pattern, input_string)
+    return phone_numbers
 
 
 # Hide all numbers in a phone number except the last 4 digits. An example of this looks like: XXX-XXX-1234
 def hide_phone_numbers(input_string):
-    pass
+    pattern = "\d{3}.\d{3}"
+    return re.sub(pattern, "XXX-XXX", input_string, count=0)
 
 
 # Get the string of the phone number and format it for our pretend application. Ensure all of the phone numbers use dashes for delimiters.
